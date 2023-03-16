41d565e69f6df9679b6064d07866a9b91bae1c8e
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-03-16 13:46:26 +1300

    Day 8 - Multithreaded

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,33 +1,42 @@
 use itertools::Itertools;
+use rayon::prelude::*;
 
 const INPUT: &str = include_str!("../input.txt");
 const SHOPS: usize = 12;
 
 fn main() {
-    let shops: Vec<usize> = (1..SHOPS).collect();
-    let combinations: Vec<_> = shops.iter().permutations(shops.len()).collect();
-    let mut matrix = [[0; SHOPS]; SHOPS];
+    let start = std::time::Instant::now();
 
+    let shops: Vec<usize> = (1..SHOPS).collect();
+    let mut matrix = [[0_u32; SHOPS]; SHOPS];
+
+    // Parse input into `matrix`
     for (y, row) in INPUT.lines().enumerate() {
         for (x, cell) in row.split_whitespace().enumerate() {
             matrix[x][y] = cell.parse().unwrap();
         }
     }
 
-    let mut distances = Vec::with_capacity(combinations.len());
+    let mut combinations: Vec<_> = shops.iter().permutations(shops.len()).collect();
 
-    for combination in combinations.iter() {
-        let mut distance = 0;
-        let mut combination = combination.clone();
-        combination.insert(0, &0);
-        combination.push(&0);
-        for window in combination.windows(2) {
-            let last = *window[0];
-            let n = *window[1];
-            distance += matrix[last][n];
-        }
-        distances.push(distance);
-    }
+    let min = combinations
+        .par_iter_mut()
+        .map(|c| {
+            c.insert(0, &0);
+            c.push(&0);
+            let mut distance = 0;
+            for window in c.windows(2) {
+                let last = *window[0];
+                let n = *window[1];
+                distance += matrix[last][n];
+            }
+            distance
+        })
+        .min()
+        .unwrap();
 
-    dbg!(distances.iter().min());
+    let duration = start.elapsed();
+
+    println!("Min Distance: {}", min);
+    println!("Found In: {:.2?}", duration);
 }
